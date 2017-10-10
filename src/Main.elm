69d26596775a0db0630ab8ef935806e34c16c505
Devon Walker <devon@winterfell.localdoamin>
2017-10-10 18:51:40 -0400

    Updated the app to handle user interaction and create new skills

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,15 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
 
 
 
 main =
-  view model
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
 
 
 -- MODEL
@@ -16,60 +21,100 @@ type alias Skill =
   , progress: String
   }
 
+type alias NewSkill =
+  { name: String
+  , description: String
+  }
+
 type alias Model =
-  List Skill
+  { skillList: List Skill
+  , newSkill: NewSkill
+  }
 
 model : Model
 model =
-  [ Skill "Skill #1" "Some supporting for a description" "50"
-  , Skill "Skill #2" "Some supporting for a description" "25"
-  ]
+  { skillList = []
+  , newSkill = NewSkill "" ""
+  }
 
 
 -- UPDATE
+
+
+type Msg
+  = UpdateName String
+  | UpdateDescription String
+  | AddSkill
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    UpdateName xName ->
+      let
+        oldSkill = model.newSkill
+
+        skill = { oldSkill | name = xName }
+      in
+        { model | newSkill = skill }
+    UpdateDescription xDescription ->
+      let
+        oldSkill = model.newSkill
+
+        skill = { oldSkill | description = xDescription }
+      in
+        { model | newSkill = skill }
+    AddSkill ->
+      let
+        newSkill = Skill (.name model.newSkill) (.description model.newSkill) (toString 0)
+
+        updatedSkillList = newSkill :: model.skillList
+      in
+        { model | skillList = updatedSkillList }
+
+
 -- VIEW
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
   div []
-    [ addSkillForm
+    [ addSkillForm model.newSkill
     , hr [] []
-    , skillList model
+    , skillList model.skillList
     ]
 
-addSkillForm : Html msg
-addSkillForm =
+addSkillForm : NewSkill -> Html Msg
+addSkillForm newSkill =
   div [ class "row" ]
     [ div [ class "col" ]
         [ Html.form []
             [ div [ class "form-group row" ]
                 [ label [ for "somethingName", class "col-sm-2 col-form-label" ] [ text "Name" ]
                 , div [ class "col-sm-10" ]
-                    [ input [ type_ "text", class "form-control", id "somethingName" ] [] ]
+                    [ input [ type_ "text", class "form-control", id "somethingName", onInput UpdateName ] [] ]
                 ]
             , div [ class "form-group row" ]
                 [ label [ for "somethingDescription", class "col-sm-2 col-form-label" ] [ text "Description" ]
                 , div [ class "col-sm-10" ]
-                    [ input [ type_ "text", class "form-control", id "somethingDescription" ] [] ]
+                    [ input [ type_ "text", class "form-control", id "somethingDescription", onInput UpdateDescription ] [] ]
                 ]
-            , button [ type_ "submit", class "btn btn-primary" ] [ text "Add" ]
+            , button [ type_ "button", class "btn btn-primary", onClick AddSkill ] [ text "Add" ]
             ]
         ]
     ]
 
-skillList : Model -> Html msg
-skillList model =
+skillList : List Skill -> Html Msg
+skillList skillList =
   div [ class "row" ]
-    [ div [ class "col" ] (List.map skillComponent model) ]
+    [ div [ class "col" ] (List.map skillComponent skillList) ]
 
-skillComponent : Skill -> Html msg
+skillComponent : Skill -> Html Msg
 skillComponent skill =
   div [ class "card" ]
     [ div [ class "card-body" ]
         [ h4 [ class "card-title" ] [ text skill.title ]
         , p [ class "card-text" ] [ text skill.description ]
         , div [ class "progress" ]
-            [ div [class "progress-bar", style [ ("width", skill.progress ++ "%") ], attribute "role" "progressbar", attribute "aria-valuenow" skill.progress, attribute "aria-valuemin" "0", attribute "aria-valuemax" "100" ] [] ]
+            [ div [ class "progress-bar", style [ ("width", skill.progress ++ "%") ], attribute "role" "progressbar", attribute "aria-valuenow" skill.progress, attribute "aria-valuemin" "0", attribute "aria-valuemax" "100" ] [] ]
         ]
     ]
