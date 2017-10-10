3c157cc9081472e6ad7c28bc97190d3ee5a7187a
Devon Walker <devon@winterfell.localdoamin>
2017-10-10 14:32:49 -0400

    Created a skill card component

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,5 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 
@@ -7,12 +8,28 @@ main =
 
 
 -- MODEL
+
+
+type alias Model =
+  { title : String
+  , description: String
+  , progress: String
+  }
+
+model =
+  Model "Skill #1" "Some supporting for a description" "50"
+
+
 -- UPDATE
 -- VIEW
 
 
 view =
-  div []
-    [ h1 [] [ text "Call your girlfriend" ]
-    , p [] [ text "This is some content" ]
+  div [ class "card" ]
+    [ div [ class "card-body" ]
+        [ h4 [ class "card-title" ] [ text model.title ]
+        , p [ class "card-text" ] [ text model.description ]
+        , div [ class "progress" ]
+            [ div [class "progress-bar", style [ ("width", model.progress ++ "%") ], attribute "role" "progressbar", attribute "aria-valuenow" model.progress, attribute "aria-valuemin" "0", attribute "aria-valuemax" "100" ] [] ]
+        ]
     ]
